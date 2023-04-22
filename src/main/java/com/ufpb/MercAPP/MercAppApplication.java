d218d754360ea5cda13f61f85b8b316ebd7715be
robinsonmourao <bob.info.guaratiba@gmail.com>
2023-04-22 08:39:45 -0300

    initial commit

---
+++ src/main/java/com/ufpb/MercAPP/MercAppApplication.java
@@ -0,0 +1,13 @@
+package com.ufpb.MercAPP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercAppApplication.class, args);
+	}
+
+}
